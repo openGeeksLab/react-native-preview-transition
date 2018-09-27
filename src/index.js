aa638591cec267d8482ef1ef543228faaa9b0de5
admin <admin@admins-iMac.local>
2018-09-27 11:50:07 +0300

    card render wip

--- src/index.js
+++ src/index.js
@@ -17,8 +17,74 @@ import { cardHeigth, cardWidth } from './values';
 export default class App extends Component {
   static defaultProps = {
     data: [],
-    animation: new Animated.Value(0),
   }
+
+  constructor() {
+    super();
+    this.state = {
+      currentIndex: null,
+      animationValue: new Animated.Value(0),
+    };
+    this.cardRef = [];
+  }
+
+  configureOpenStyle = () => {
+    const { animationValue, currentViewInfo } = this.state;
+    /*
+      currentViewInfo: {
+        fx, fy, width, height, px, py,
+      }
+    */
+    return {
+      height: animationValue.interpoalte({
+        inputRange: [0, 1],
+        outputRange: []
+      }),
+      width: '',
+
+    }
+    /*
+      0.0
+
+      300 - height of card
+
+      height
+    */
+
+  }
+
+  closeCard = () => {
+    const { currentIndex } = this.state;
+    if (currentIndex !== null) {
+      this.setState({ currentIndex: null });
+    }
+  }
+
+  openCard = (index) => {
+    const { data } = this.props;
+    const cardReference = this.cardRef[`card-ref-${index}`];
+    if (cardReference && cardReference.measure && index !== undefined) {
+      const currentCardData = data[index];
+      if (currentCardData) {
+        cardReference.measure((fx, fy, width, height, px, py) => {
+          this.setState({
+            currentIndex: index,
+            currentViewInfo: {
+              fx, fy, width, height, px, py,
+            },
+          });
+        });
+      }
+    }
+  }
+
+  onOpenCard = (screenIndex) => {
+    this.openCard(screenIndex);
+  }
+
+  // animationStart = () => {
+  //
+  // }
 
   renderImage = (source) => {
     if (source) {
@@ -47,7 +113,7 @@ export default class App extends Component {
 
   renderCardView = (itemData, itemIndex) => {
     return (
-      <TouchableOpacity>
+      <TouchableOpacity onPress={() => this.onOpenCard(itemIndex)}>
         <View
           style={[
             styles.cardContainer,
@@ -57,6 +123,9 @@ export default class App extends Component {
             },
           ]}
           key={`card-view-${itemIndex}`}
+          ref={(viewRef) => {
+            this.cardRef[`card-ref-${itemIndex}`] = viewRef;
+          }}
         >
           {this.renderImage(itemData.img)}
           {this.renderCardTitle(itemData, itemIndex)}
@@ -88,13 +157,59 @@ export default class App extends Component {
     );
   }
 
+  renderFullCard = () => {
+    const { data } = this.props;
+    const { currentIndex, currentViewInfo } = this.state;
+    if (currentIndex !== undefined) {
+      const currentCardData = data[currentIndex];
+      if (currentCardData) {
+        const { fx, fy, width, height, px, py } = currentViewInfo;
+        return (
+          // Контейнер раскрытой карточки.
+          <View
+            style={{
+              position: 'absolute',
+              top: 0,
+              left: 0,
+              height: '100%',
+              width: '100%',
+              backgroundColor: 'black',
+            }}
+          >
+            <TouchableOpacity onPress={this.closeCard}>
+              <View>
+                <Image
+                  // resizeMode={'contain'}
+                  source={currentCardData.img}
+                  style={{
+                    position: 'absolute',
+                    top: py,
+                    left: px,
+                    width: width,
+                    height: height,
+                  }}
+                />
+              </View>
+            </TouchableOpacity>
+            {/* <View>
+            </View> */}
+          </View>
+        );
+      }
+    }
+    return null;
+  }
+
   render() {
+    const { currentIndex } = this.state;
+
     return (
       <View style={styles.container}>
         <ScrollView
           orientation='vertical'
         >
           {this.renderCards()}
+          {this.renderFullCard(currentIndex)}
         </ScrollView>
       </View>
     );
